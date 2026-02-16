7ae46d2704e5457710764e7ecfaa02ee6548144c
Kallekiri <kiri.kalle8@gmail.com>
2026-02-16 18:52:52 +0200

    Hahmon liikutuksen konfiguraatio

---
+++ phantom/Config/InputConfig.cs
@@ -0,0 +1,8 @@
+public static class InputConfig
+{
+	public const string InputLeft = "MoveLeft";
+	public const string InputRight = "MoveRight";
+	public const string InputForward = "MoveForward";
+	public const string InputBackward = "MoveBackward";
+
+}
