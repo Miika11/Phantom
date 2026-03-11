a9d3901675d9bf8ef5e73ea0149315748c0d29ae
Miika Hast <miika.hast@tuni.fi>
2026-03-11 18:21:26 +0200

    elämäpisteet

--- phantom/Scripts/CharacterController2.cs
+++ phantom/Scripts/CharacterController2.cs
@@ -4,24 +4,37 @@ public partial class CharacterController2 : CharacterBody2D
 {
     [Export]
     public float Speed = 100.0f;
+
     private Vector2 _Movement = Vector2.Zero;
     private AnimatedSprite2D _player;
+
+    [Export]
+    public int MaxHealth = 3;
+
+    private int _currentHealth;
 
     public override void _Ready()
     {
         _player = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        
+        _currentHealth = MaxHealth;
+
+        GD.Print("Elämät: " + _currentHealth);
     }
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
 
     public override void _PhysicsProcess(double delta)
     {
-        _Movement = Input.GetVector(ConfigInput.InputLeft, ConfigInput.InputRight, ConfigInput.InputForward, ConfigInput.InputBackward);
-    //    GD.Print("Movement: " + _Movement);
+        _Movement = Input.GetVector(
+            ConfigInput.InputLeft,
+            ConfigInput.InputRight,
+            ConfigInput.InputForward,
+            ConfigInput.InputBackward
+        );
 
         Vector2 velocity;
 
-
-        if(!Mathf.IsZeroApprox(_Movement.Length()))
+        if (!Mathf.IsZeroApprox(_Movement.Length()))
         {
             velocity = _Movement.Normalized() * Speed;
         }
@@ -55,5 +68,26 @@ public partial class CharacterController2 : CharacterBody2D
             else
                 _player.Play(ConfigAnimation.MoveUp);
         }
+    }
+
+    
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+
+        GD.Print("Elämät: " + _currentHealth);
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Scene alkaa alusta kuollessa
+    private void Die()
+    {
+        GD.Print("Pelaaja kuoli!");
+
+        GetTree().ReloadCurrentScene();
     }
 }
