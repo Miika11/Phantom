e47e2c1ec49cefa161e5da13757bd23ec2998485
Miika Hast <miika.hast@tuni.fi>
2026-03-11 18:41:25 +0200

    avainanimaatio

--- phantom/Scripts/Key.cs
+++ phantom/Scripts/Key.cs
@@ -1,15 +1,26 @@
-using Godot;
 using System;
-
+using Godot;
 
 public partial class Key : Collectable
 {
-	[Export] private int _score = 1;
+    [Export] private int _score = 1;
 
-	protected override void Collect(CharacterController2 characterController2)
-	{
-		GD.Print($"Kerättiin avain: {_score}");
-		GameManager.Instance.AddScore(_score);
-	}
+    private AnimatedSprite2D _sprite;
 
+    public override void _Ready()
+    {
+        
+        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        
+        _sprite.Play("Idle"); 
+    }
+
+    protected override void Collect(CharacterController2 characterController2)
+    {
+        GD.Print($"Kerättiin avain: {_score}");
+        GameManager.Instance.AddScore(_score);
+
+        QueueFree(); 
+    }
 }
