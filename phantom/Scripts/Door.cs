d4855e7d5155b6a5c03ed0a371e79b3c31304b3f
Miika Hast <miika.hast@tuni.fi>
2026-03-22 15:11:54 +0200

    DoorcsBugFix

--- phantom/Scripts/Door.cs
+++ phantom/Scripts/Door.cs
@@ -3,11 +3,14 @@ using System;
 
 public partial class Door : Area2D
 {
-	[Export]
-	public string TargetScene {get; set;} = "";
-	[Export]
-	public string TargetSpawnPoint {get; set;} = "StartSpawn";
-	private bool _opened = false;
+    [Export]
+    public string TargetScene { get; set; } = "";
+
+    [Export]
+    public string TargetSpawnPoint { get; set; } = "StartSpawn";
+
+    private bool _opened = false;
+
     public override void _EnterTree()
     {
         BodyEntered += OnBodyEntered;
@@ -17,14 +20,21 @@ public partial class Door : Area2D
     {
         BodyEntered -= OnBodyEntered;
     }
-	private void OnBodyEntered(Node2D body)
-	{
 
-		if (body is CharacterBody2D && TargetScene != "" && !_opened)
-		{
-			_opened = true;
-			GameManager.Instance.SpawnPoint = TargetSpawnPoint;
-			GetTree().ChangeSceneToFile(TargetScene);
-		}
-	}
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is CharacterBody2D && TargetScene != "" && !_opened)
+        {
+            _opened = true;
+            GameManager.Instance.SpawnPoint = TargetSpawnPoint;
+
+            // Defer scene change
+            CallDeferred(nameof(ChangeScene));
+        }
+    }
+
+    private void ChangeScene()
+    {
+        GetTree().ChangeSceneToFile(TargetScene);
+    }
 }
