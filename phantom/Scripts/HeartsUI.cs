10787acd05fb2777ea996f2921a4b7abb3ad3c6d
Miika Hast <miika.hast@tuni.fi>
2026-03-24 16:01:38 +0200

    UIcomments

--- phantom/Scripts/HeartsUI.cs
+++ phantom/Scripts/HeartsUI.cs
@@ -6,7 +6,7 @@ public partial class HeartsUI : HBoxContainer
     [Export] public Texture2D FullHeart;
     [Export] public Texture2D EmptyHeart;
 
-    private List<TextureRect> _hearts = new List<TextureRect>();
+    private List<TextureRect> _hearts = new List<TextureRect>(); // list to store all health ui elements
 
     public override void _Ready()
     {
@@ -20,10 +20,10 @@ public partial class HeartsUI : HBoxContainer
 
     public void UpdateHearts(int currentHealth)
     {
-        for (int i = 0; i < _hearts.Count; i++)
+        for (int i = 0; i < _hearts.Count; i++) // Go through all the hearts
         {
             if (i < currentHealth)
-                _hearts[i].Texture = FullHeart;
+                _hearts[i].Texture = FullHeart; //if index less than current health show full heart else empty heart
             else
                 _hearts[i].Texture = EmptyHeart;
         }
--- phantom/Scripts/KeyUI.cs
+++ phantom/Scripts/KeyUI.cs
@@ -2,18 +2,18 @@ using Godot;
 
 public partial class KeyUI : HBoxContainer
 {
-    private Label _label;
+    private Label _label; // Reference to the label that shows key amount
 
     public override void _Ready()
     {
         
-        _label = GetNode<Label>("KeyLabel");
+        _label = GetNode<Label>("KeyLabel"); // gets the label node from the scene
         // GD.Print("_label: ", _label); // for debugging
     }
 
     public void UpdateKeys(int amount)
     {
         if (_label != null) 
-            _label.Text = "x" + amount;
+            _label.Text = "x" + amount; // updates how many keys player has collected
     }
 }
