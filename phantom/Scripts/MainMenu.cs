c0882a8c93fd04ef2f1cd3e48ca90d5c00a442b6
Miika Hast <miika.hast@tuni.fi>
2026-04-21 18:52:52 +0300

    creditsmenu

--- phantom/Scripts/MainMenu.cs
+++ phantom/Scripts/MainMenu.cs
@@ -7,6 +7,13 @@ public partial class MainMenu : CanvasLayer
     [Export] public TextureButton _settings;
     [Export] public TextureButton _credits;
     [Export] public CanvasLayer _settingsmenu;
+    [Export] public CanvasLayer _creditsmenu;
+
+    [Export] public TextureButton _creditsClose;
+
+    [Export] public Texture2D _creditsImageFI;
+    [Export] public Texture2D _creditsImageEN;
+    [Export] public TextureRect _creditsImage;
 
     [Export] public Texture2D _playFI;
     [Export] public Texture2D _quitFI;
@@ -18,39 +25,46 @@ public partial class MainMenu : CanvasLayer
     [Export] public Texture2D _settingsEN;
     [Export] public Texture2D _creditsEN;
 
-	public override void _Ready()
-	{
+    public override void _Ready()
+    {
         GetNode<MusicManager>("/root/MusicManager")
-        .PlayMusic("res://audio/MainMenu.ogg");
-		GetTree().CallGroup("GameHUD", "hide");
-		_settingsmenu.Visible = false;
-		_settings.Pressed += OnSettingsPressed;
-		_play.Pressed += OnPlayPressed;
-		_quit.Pressed += OnQuitPressed;
+            .PlayMusic("res://audio/MainMenu.ogg");
 
-		GlobalSettings.Instance.LocaleChanged += RefreshTextures;
-		RefreshTextures();
-	}
+        GetTree().CallGroup("GameHUD", "hide");
 
+        _settingsmenu.Visible = false;
+        _creditsmenu.Visible = false;
 
-	public override void _ExitTree()
-	{
-    GlobalSettings.Instance.LocaleChanged -= RefreshTextures;
-}
+        _settings.Pressed += OnSettingsPressed;
+        _play.Pressed += OnPlayPressed;
+        _quit.Pressed += OnQuitPressed;
+        _credits.Pressed += OnCreditsPressed;
+        _creditsClose.Pressed += OnCreditsClosePressed;
+
+        GlobalSettings.Instance.LocaleChanged += RefreshTextures;
+        RefreshTextures();
+    }
+
+    public override void _ExitTree()
+    {
+        GlobalSettings.Instance.LocaleChanged -= RefreshTextures;
+    }
 
     private void RefreshTextures()
-{
-    bool isFinnish = GlobalSettings.Instance.GetLocale() == "fi";
-    _play.TextureNormal = isFinnish ? _playFI : _playEN;
-    _quit.TextureNormal = isFinnish ? _quitFI : _quitEN;
-    _settings.TextureNormal = isFinnish ? _settingsFI : _settingsEN;
-    _credits.TextureNormal = isFinnish ? _creditsFI : _creditsEN;
-}
-
-    private void OnSettingsPressed()
     {
-        _settingsmenu.Visible = true;
+        bool isFinnish = GlobalSettings.Instance.GetLocale() == "fi";
+        _play.TextureNormal = isFinnish ? _playFI : _playEN;
+        _quit.TextureNormal = isFinnish ? _quitFI : _quitEN;
+        _settings.TextureNormal = isFinnish ? _settingsFI : _settingsEN;
+        _credits.TextureNormal = isFinnish ? _creditsFI : _creditsEN;
+
+        if (_creditsImage != null)
+            _creditsImage.Texture = isFinnish ? _creditsImageFI : _creditsImageEN;
     }
+
+    private void OnSettingsPressed() => _settingsmenu.Visible = true;
+    private void OnCreditsPressed() => _creditsmenu.Visible = true;
+    private void OnCreditsClosePressed() => _creditsmenu.Visible = false;
 
     private void OnPlayPressed()
     {
@@ -59,9 +73,4 @@ public partial class MainMenu : CanvasLayer
     }
 
     private void OnQuitPressed() => GetTree().Quit();
-
-    // private void OnCreditsPressed()
-    // {
-    //     GetTree().ChangeSceneToFile("res://Scenes/Credits.tscn");
-    // }
 }
